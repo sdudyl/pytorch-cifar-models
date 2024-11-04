5b54aed8da1632dcdd1a566e38514de84be9a841
DongYilin <71010520+sdudyl@users.noreply.github.com>
2024-11-04 22:49:53 +0800

    Update resnet.py

--- pytorch_cifar_models/resnet.py
+++ pytorch_cifar_models/resnet.py
@@ -153,6 +153,7 @@ class CifarResNet(nn.Module):
         self.layer1 = self._make_layer(block, 16, layers[0], layer_num=1)
         self.layer2 = self._make_layer(block, 32, layers[1], layer_num=2, stride=2)
         self.layer3 = self._make_layer(block, 64, layers[2], layer_num=3, stride=2)
+        self.dylreset=counter.reset()
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(64 * block.expansion, num_classes)
