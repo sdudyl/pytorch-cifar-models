b765b34724b1bb90cfaa5d1937296e6e51b74e6b
DongYilin <71010520+sdudyl@users.noreply.github.com>
2024-11-18 19:36:46 +0800

    Update resnet.py

--- pytorch_cifar_models/resnet.py
+++ pytorch_cifar_models/resnet.py
@@ -1,48 +1,182 @@
-import torch
-import torchvision
-import torchvision.transforms as transforms
 
-# 定义数据预处理
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-])
+import sys
+import torch.nn as nn
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
-# 加载 CIFAR-10 测试集
-full_testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                             download=True, transform=transform)
+from functools import partial
+from typing import Dict, Type, Any, Callable, Union, List, Optional
 
-# 创建测试集的 DataLoader
-testloader = torch.utils.data.DataLoader(full_testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+from myrelu import cheby_relu
 
 
-# 加载预训练模型
-model = torch.hub.load("sdudyl/pytorch-cifar-models", "cifar10_resnet20", pretrained=True, force_reload=True)
-#model = torch.hub.load("/home/dyl/241114/pytorch-cifar-models/", "cifar10_resnet20", pretrained=True, force_reload=True)
-model.eval()
+cifar10_pretrained_weight_urls = {
+    'resnet20': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar10_resnet20-4118986f.pt',
+    'resnet32': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar10_resnet32-ef93fc4d.pt',
+    'resnet44': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar10_resnet44-2a3cabcb.pt',
+    'resnet56': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar10_resnet56-187c023a.pt',
+}
+
+cifar100_pretrained_weight_urls = {
+    'resnet20': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar100_resnet20-23dac2f1.pt',
+    'resnet32': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar100_resnet32-84213ce6.pt',
+    'resnet44': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar100_resnet44-ffe32858.pt',
+    'resnet56': 'https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar100_resnet56-f2eff4c8.pt',
+}
 
 
-# 评估模型的正确性，只测试前1000张图片
-correct = 0
-total = 0
-max_images = 1000  # 设定要测试的图片数量上限
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
 
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = model(images)
-        _, predicted = torch.max(outputs, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
 
-        # 检查已处理图片是否达到上限
-        if total >= max_images:
-            total = max_images  # 确保总数精确为1000
-            break
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
-accuracy = 100 * correct / total
-print(f'Accuracy of the model on the first {max_images} CIFAR-10 test images: {accuracy:.2f}%')
+
+# 替代的 ReLU 类
+class CustomReLU(nn.Module):
+    def __init__(self):
+        super(CustomReLU, self).__init__()
+
+    def forward(self, x):
+        return cheby_relu(x)
 
 
 
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        # self.relu = nn.ReLU(inplace=True)
+        self.relu = CustomReLU()  # 替换为自定义 ReLU
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class CifarResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=10):
+        super(CifarResNet, self).__init__()
+        self.inplanes = 16
+        self.conv1 = conv3x3(3, 16)
+        self.bn1 = nn.BatchNorm2d(16)
+        # self.relu = nn.ReLU(inplace=True)
+        self.relu = CustomReLU()  # 替换为自定义 ReLU
+
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def _resnet(
+    arch: str,
+    layers: List[int],
+    model_urls: Dict[str, str],
+    progress: bool = True,
+    pretrained: bool = False,
+    **kwargs: Any
+) -> CifarResNet:
+    model = CifarResNet(BasicBlock, layers, **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls[arch],
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+    return model
+
+
+def cifar10_resnet20(*args, **kwargs) -> CifarResNet: pass
+def cifar10_resnet32(*args, **kwargs) -> CifarResNet: pass
+def cifar10_resnet44(*args, **kwargs) -> CifarResNet: pass
+def cifar10_resnet56(*args, **kwargs) -> CifarResNet: pass
+
+
+def cifar100_resnet20(*args, **kwargs) -> CifarResNet: pass
+def cifar100_resnet32(*args, **kwargs) -> CifarResNet: pass
+def cifar100_resnet44(*args, **kwargs) -> CifarResNet: pass
+def cifar100_resnet56(*args, **kwargs) -> CifarResNet: pass
+
+
+thismodule = sys.modules[__name__]
+for dataset in ["cifar10", "cifar100"]:
+    for layers, model_name in zip([[3]*3, [5]*3, [7]*3, [9]*3],
+                                  ["resnet20", "resnet32", "resnet44", "resnet56"]):
+        method_name = f"{dataset}_{model_name}"
+        model_urls = cifar10_pretrained_weight_urls if dataset == "cifar10" else cifar100_pretrained_weight_urls
+        num_classes = 10 if dataset == "cifar10" else 100
+        setattr(
+            thismodule,
+            method_name,
+            partial(_resnet,
+                    arch=model_name,
+                    layers=layers,
+                    model_urls=model_urls,
+                    num_classes=num_classes)
+        )
